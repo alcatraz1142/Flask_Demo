3841530da77ae412f071a8993f86c743afa0c027
alcatraz1142 <kkordzi@wp.pl>
2026-04-14 17:30:29 +0200

    add login

--- routes/login.py
+++ routes/login.py
@@ -1,3 +1,5 @@
+import token
+
 from flask import Blueprint, request, jsonify
 import sqlite3
 from security.auth import create_token
@@ -38,6 +40,43 @@ def login_vulnerable():
 
     token = create_token(user_id, role)
     return jsonify({'access_token': token, 'token_type': 'bearer'}), 200
+
+
+@login_bp.post("/login_safe")
+def login_safe():
+    data = request.get_json()
+
+    username = data['username']
+    password = data['password']
+
+    if not username or not password:
+        return jsonify({"error": "Missing username or password"}), 400
+
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+
+    query = """
+     SELECT id, username, password, role
+        FROM users
+        WHERE username = ?
+    """
+
+    cursor.execute(query, (username,))
+    row = cursor.fetchone()
+    conn.close()
+
+    if not row:
+        return jsonify({"error": "Invalid credentials (safe)"}), 401
+
+    user_id, db_username, db_password_hash, role = row
+
+    if not verify_password(password, db_password_hash):
+        return jsonify({"error": "Invalid credentials (safe)"}), 401
+
+    token = create_token(user_id, role)
+    return jsonify({'access_token': token, 'token_type': 'bearer'}), 200
+
+
 
 
 
