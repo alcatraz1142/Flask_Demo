201955e2d6242bfd9415bbeb04af925d47c003eb
alcatraz1142 <kkordzi@wp.pl>
2026-04-21 13:05:58 +0200

    add vulnreable IDOR and IDOR-proof

--- routes/secure_routes.py
+++ routes/secure_routes.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import Blueprint, jsonify
 from utils.token_required import token_required
 
@@ -23,3 +25,49 @@ def admin_only(decoded_token):
 
 
 
+@secure_bp.route('/user-vulnerable/<int:user_id>', methods=['GET'])
+@token_required
+def vulnerable_user(decoded_token, user_id):
+
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, username, role FROM users WHERE id = ?", (user_id,))
+    row = cursor.fetchone()
+    conn.close()
+
+    if not row:
+        return jsonify({"message": "User not found"}), 404
+
+    user_id, username, role = row
+    return jsonify({
+        "id": user_id,
+        "username": username,
+        "role": role
+    })
+
+
+@secure_bp.route('/user-safe/<int:user_id>', methods=['GET'])
+@token_required
+def user_secure(decoded_token, user_id):
+    requester_id = int(decoded_token.get("sub"))
+    requester_role = decoded_token.get("role")
+
+    if requester_id != user_id and requester_role != "admin":
+        return jsonify({"error": "Access denied"}), 403
+
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, username, role FROM users WHERE id = ?", (user_id,))
+    row = cursor.fetchone()
+    conn.close()
+
+    if not row:
+        return jsonify({"message": "User not found"}), 404
+
+    user_id, username, role = row
+    return jsonify({
+        "id": user_id,
+        "username": username,
+        "role": role
+    })
