d463bb7add493777689d6f509b7c3ca75305afe1
alcatraz1142 <kkordzi@wp.pl>
2026-04-21 13:38:18 +0200

    add XSS endpoint

--- routes/public.py
+++ routes/public.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+import html
+from flask import Blueprint, jsonify, request
 
 public_bp = Blueprint('public', __name__)
 
@@ -6,3 +7,14 @@ public_bp = Blueprint('public', __name__)
 def public():
     return jsonify({'message': 'Public endpoint - no authentication required'})
 
+@public_bp.route('/xss-vulnereable', methods=['GET'])
+def xss_vulnereable():
+    value = request.args.get("q", "")
+    #zwrócenie wartości bez żadnej walidacji
+    return f"<h1>Echo: {value}</h1>"
+
+@public_bp.route('/xss-safe', methods=['GET'])
+def xss_safe():
+    value = request.args.get("q", "")
+    safe_value = html.escape(value)
+    return f"<h1>Echo: {safe_value}</h1>"
