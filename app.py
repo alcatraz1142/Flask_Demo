6f5f6bf6b78e71591346015cc11265788b9ba59d
alcatraz1142 <kkordzi@wp.pl>
2026-04-21 13:16:30 +0200

    add headers and middleware

--- app.py
+++ app.py
@@ -11,6 +11,24 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET
 
+@app.after_request
+def after_request(response):
+    #ochrona przed click hijacking
+    response.headers["X-Frame-Options"] = "DENY"
+
+    #ochrona przed MIME Sniffing
+    response.headers["X-Content-Type-Options"] = "nosniff"
+
+    #ochrona przed XSS w starszych przeglądarkach
+    response.headers["X-XSS-Protection"] = "1; mode=block"
+
+    #strict transport security (HTTPS)
+    response.headers['Strict-Transport-Security'] = "max-age=63072000; includeSubDomains; preload"
+
+    #Content Security Policy
+    response.headers['Content-Security-Policy'] = "default-src 'self';"
+
+    return response
 
 
 app.register_blueprint(public_bp)
